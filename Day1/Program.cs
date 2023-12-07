bb1eb39f522e27e545f74f970b703374173a6385
Gilbike <marton.dikacz@gmail.com>
2023-12-07 20:16:16 +0100

    Add project files.

---
+++ Day1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Text.RegularExpressions;
+
+string[] lines = File.ReadAllLines("input.txt");
+
+int sum = 0;
+foreach (string line in lines)
+{
+    int firstIndex = 0;
+    int secondIndex = line.Length - 1;
+
+    while (firstIndex < line.Length && !int.TryParse(line[firstIndex].ToString(), out _))
+    {
+        firstIndex++;
+    }
+
+    while (secondIndex > 0 && !int.TryParse(line[secondIndex].ToString(), out _))
+    {
+        secondIndex--;
+    }
+
+    string twoDigits = line[firstIndex].ToString() + line[secondIndex].ToString();
+    sum += int.Parse(twoDigits);
+}
+
+Console.WriteLine(sum);
